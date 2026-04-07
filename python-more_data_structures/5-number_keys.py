926ff9f4969510a885244782f6eb7919f5cc5499
HuseynliTurane <13612@holbertonschool.com>
2026-04-07 07:58:33 +0000

    task5

---
+++ python-more_data_structures/5-number_keys.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def number_keys(a_dictionary):
+    """
+    Returns the number of keys in a dictionary.
+    """
+    # len() funksiyası lüğətdəki açarların sayını qaytarır
+    return len(a_dictionary)

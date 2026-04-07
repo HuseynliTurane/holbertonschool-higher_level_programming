57f039d51f952d676c3fa37c59f75106e39483f2
HuseynliTurane <13612@holbertontudents.com>
2026-04-07 15:06:07 +0000

    task1

---
+++ python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Square module
+"""
+
+
+class Square:
+    """
+    Square class
+    """
+
+    def __init__(self, size):
+        """
+        Init square
+        """
+        self.__size = size

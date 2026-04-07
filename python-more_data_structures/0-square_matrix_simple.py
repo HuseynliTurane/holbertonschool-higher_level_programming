f716eae1aaa1a333abe55ea83c33428d3b9d8aa1
HuseynliTurane <13612@holbertonschool.com>
2026-04-07 07:42:34 +0000

    task000

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    return [[element ** 2 for element in row] for row in matrix]
-
+    """Computes the square value of all integers of a matrix."""
+    return [[x**2 for x in row] for row in matrix]
+# <--- Burada yalnız bir boş sətir olmalıdır, vəssalam.

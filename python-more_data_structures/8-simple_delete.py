15507cc26b00783b8a1ba92ec2477272322a617c
HuseynliTurane <13612@holbertonschool.com>
2026-04-07 08:19:19 +0000

    task8

---
+++ python-more_data_structures/8-simple_delete.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def simple_delete(a_dictionary, key=""):
+    if key in a_dictionary: # Əgər açar lüğətdə varsa
+        del a_dictionary[key] # Onu sil
+    return a_dictionary # Hər iki halda lüğəti geri qaytar

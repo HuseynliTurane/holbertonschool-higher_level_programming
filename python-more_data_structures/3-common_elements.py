4be281bb63772e9e68a6b5d17f127d46ecb813da
HuseynliTurane <13612@holbertonschool.com>
2026-04-07 07:52:53 +0000

    task3

---
+++ python-more_data_structures/3-common_elements.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def common_elements(set_1, set_2):
+    """
+    Returns a set of common elements in two sets.
+    Uses the intersection operator '&'.
+    """
+    # '&' operatoru hər iki çoxluqda olan ortaq elementləri seçir
+    return set_1 & set_2

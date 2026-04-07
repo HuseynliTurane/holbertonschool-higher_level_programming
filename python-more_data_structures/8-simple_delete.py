0ecb4461ef91540611fc54876c5db0d8df25b3e7
HuseynliTurane <13612@holbertonschool.com>
2026-04-07 08:22:52 +0000

    task8

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    if key in a_dictionary: # Əgər açar lüğətdə varsa
-        del a_dictionary[key] # Onu sil
-    return a_dictionary # Hər iki halda lüğəti geri qaytar
-#111
+    """Deletes a key in a dictionary."""
+    if key in a_dictionary:
+        del a_dictionary[key]
+    return a_dictionary

3a46d86c73162bc95198277ed1cf4012c67df361
HuseynliTurane <13612@holbertonstudents.com>
2026-04-14 10:00:07 +0000

    task

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-
 def safe_print_list(my_list=[], x=0):
+    """
+    Siyahıdan x sayda elementi təhlükəsiz şəkildə çap edir.
+    Returns: Çap olunan elementlərin real sayı.
+    """
     count = 0
-
     for i in range(x):
         try:
-            print(my_list[i], end="")
+            print("{}".format(my_list[i]), end="")
             count += 1
-        except:
+        except IndexError:
             break
-
-    print()
+    print("")
     return count

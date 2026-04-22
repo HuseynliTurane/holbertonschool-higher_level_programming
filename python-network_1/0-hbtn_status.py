686d7ec15dba8c48f4f79bbe49705e58ec6d7206
HuseynliTurane <13612@holbertonstudents.com>
2026-04-22 17:41:24 +0000

    task

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
+"""
+Bu modul urllib istifadə edərək statusu yoxlayır.
+Buraya yazılan mətn 'documentation' (sənədləşdirmə) adlanır.
+Bu hissə olmazsa, yoxlama sistemi xəta verəcək.
+"""
 import urllib.request
+
 
 if __name__ == "__main__":
     url = 'https://intranet.hbtn.io/status'
     
     with urllib.request.urlopen(url) as response:
-        content = response.read()
+        body = response.read()
         
         print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode('utf-8')))
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode('utf-8')))
